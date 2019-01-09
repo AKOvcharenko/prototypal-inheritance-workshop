d283f41ae7a47bce1f9c4adc4b74cbd0e588d27f
artem.ovcharenko <artem.ovcharenko@codilime.com>
2019-01-09 12:17:26 +0100

    01 solution

--- exercises/01-singleton/01-singleton.js
+++ exercises/01-singleton/01-singleton.js
@@ -2,4 +2,15 @@
  * TODO: create a constructor named `Singletone`
  */
 
+var Singleton = (function creator() {
+  var instance;
+  return function () {
+    if (!instance) {
+      instance = this;
+      return this;
+    }
+    return instance;
+  }
+})();
+
 module.exports = Singleton;
