792e89a361f012c2dfa54db551e03abacae1cee8
artem.ovcharenko <artem.ovcharenko@codilime.com>
2019-01-10 21:58:10 +0100

    solution for 02

--- exercises/02-native prototypes/02-native-prototypes.js
+++ exercises/02-native prototypes/02-native-prototypes.js
@@ -1,6 +1,13 @@
 /**
  * TODO: object's method toString should returns `Big Brother watching You! ${previous result}`
  */
+
+Object.prototype.toString = (function (old) {
+  return  function () {
+    return 'Big Brother watching You! ' + old.call(this);
+  };
+})(Object.prototype.toString);
+
 module.exports = Object;
 
 
@@ -8,6 +15,7 @@ module.exports = Object;
 * TODO: Add to the prototype of all functions the method defer(ms), that runs the function after ms milliseconds.
 *
 * https://plnkr.co/edit/rGY3uNRBYiNJPsgi99OA?p=preview
+* solution https://plnkr.co/edit/VQ4Xreq4uc3t5ji5KmDr?p=preview
 * */
 
 
