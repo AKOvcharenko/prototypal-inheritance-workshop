d571f4f49206f8c3dcba8f709ddfdfa3ac9ab7e2
artem.ovcharenko <artem.ovcharenko@codilime.com>
2019-01-13 19:08:38 +0100

    update

--- exercises/05-berserker/05-berserker.js
+++ exercises/05-berserker/05-berserker.js
@@ -1,10 +1,10 @@
 /*
 *
 * TODO 1. Create Class Berserker which takes arguments: team, name, power, exp.
-* TODO 2. Wuss Class instance should inherits SuperHero Class, and should has methods:
+* TODO 2. Berserker Class instance should inherits SuperHero Class, and should has methods:
     - greet: returns string - hello there! my name is `name`, and I am `team`'s team member.
-    - kickAsses: returns string - I can not kick asses :'(, my power is `power`.
-    - manageTheTeam: returns string - I manage `team` team, with experience `exp`.
+    - kickAsses: returns string - I kick asses with power `power`.
+    - manageTheTeam: returns string - I can not manage team, I am too strong for this, my power `power`.
 
     usage example var hulk = new Berserker('avengers', 'Hulk', 1000, 1);
 * */
